10c9629a6bc749efde7cfd60d43bbf91e44e71a0
kmooney <kmooney@gmail.com>
2015-07-07 22:15:46 -0500

    Changed restlite to pass a reader for the requestbody to the handlers

--- src/gameboard/src/restlite/restlite.go
+++ src/gameboard/src/restlite/restlite.go
@@ -5,32 +5,34 @@ import (
     "net/url"
     "fmt"
     "encoding/json"
+    "io"
+
 )
 
 type Resource interface { 
-    Get(values url.Values) (int, interface{})
-    Post(values url.Values) (int, interface{})
-    Put(values url.Values) (int, interface{})
-    Delete(values url.Values) (int, interface{})
+    Get(values url.Values, body io.Reader) (int, interface{})
+    Post(values url.Values, body io.Reader) (int, interface{})
+    Put(values url.Values, body io.Reader) (int, interface{})
+    Delete(values url.Values, body io.Reader) (int, interface{})
 }
 
 type GetNotSupported struct {}
-func (GetNotSupported) Get(values url.Values) (int, interface{}) { 
+func (GetNotSupported) Get(values url.Values, body io.Reader) (int, interface{}) { 
     return 405, ""
 }
 
 type PostNotSupported struct {}
-func (PostNotSupported) Post(values url.Values) (int, interface{}) {
+func (PostNotSupported) Post(values url.Values, body io.Reader) (int, interface{}) {
     return 405, ""
 }
 
 type PutNotSupported struct {}
-func (PutNotSupported) Put(values url.Values) (int, interface{}) { 
+func (PutNotSupported) Put(values url.Values, body io.Reader) (int, interface{}) { 
     return 405, ""
 }
 
 type DeleteNotSupported struct {}
-func (DeleteNotSupported) Delete(values url.Values) (int, interface{}) { 
+func (DeleteNotSupported) Delete(values url.Values, body io.Reader) (int, interface{}) { 
     return 405, ""
 }
 
@@ -53,19 +55,25 @@ func (api *API) requestHandler(resource Resource) http.HandlerFunc {
     return func(rw http.ResponseWriter, request *http.Request) { 
         var data interface{}
         var code int
+        var err error
 
         request.ParseForm()
+        fmt.Println(request)
         method := request.Method
         values := request.Form
+
+
+        // TODO if values are empty, and method is post, 
+        // use the body of the post.
         switch method { 
             case "GET":
-                code, data = resource.Get(values)
+                code, data = resource.Get(values, request.Body)
             case "POST":
-                code, data = resource.Post(values)
+                code, data = resource.Post(values, request.Body)
             case "PUT":
-                code, data = resource.Put(values)
+                code, data = resource.Put(values, request.Body)
             case "DELETE":
-                code, data = resource.Delete(values)
+                code, data = resource.Delete(values, request.Body)
             default:
                 api.Abort(rw, 405)
         } 
